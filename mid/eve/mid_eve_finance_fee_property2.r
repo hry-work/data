4504d5046ab9906aead33fdc9f4085d8b6ab3e4d
hry-work <18137813799@163.com>
2020-08-13 14:11:16 +0800

    d

--- mid/eve/mid_eve_finance_fee_property2.r
+++ mid/eve/mid_eve_finance_fee_property2.r
@@ -1,93 +1,101 @@
 source('C:/Users/Administrator/data/env.r' , encoding = 'utf8')
 
 author <- c('huruiyi')
-table <- 'mid_eve_finance_fee_property'      # ??ҵ???¼??���???????ģ?
-# ???????򣺱?Ӧ??????_??????_????????_ҵ??????_????ϸ????
+table <- 'mid_eve_finance_fee_property'      # 物业费事件表(已修正的)
+# 命名规则：表应归属库_表类型_归属部门_业务大类_表详细归类
 
 
-# ????ĩend <- as_date(paste0(year(day) , '-12-31'))
+# 本年末
+end <- as_date(paste0(year(day) , '-12-31'))
 
-# -------------------- ׼?????????? --------------------
+# -------------------- 准备基础数据 --------------------
 
-# ---------- ??ҵ??code
+# ---------- 物业费code
 property_code <- dbGetQuery(con_orc , glue("select distinct pk_projectid , projectcode , projectname
                                             from wy_bd_fmproject 
                                             where projectcode in ('001','002','003','004')"))
 
-# ---------- ??Ŀ????????
+# ---------- 项目基础数据
 basic_info <- sqlQuery(con_sql , "select pk_house , house_name , pk_floor , floor_name , 
                                   pk_unit , unit_name , pk_build , build_name , 
                                   pk_project , project_name , pk_client , client_name
                                   from dim_owner_basic_info")
 
-# ---------- ??Ŀ????????
+# ---------- 项目归属区域
 belong <- dbGetQuery(con_orc , glue("select porject1 , porject2 , porject3 , porject4 , porject6 , wy_cycle
                                     from xywy_project"))
 
-# ---------- ?շ?????
+# ---------- 收费类型
 gathering_type <- dbGetQuery(con_orc , glue("select pk_gatheringtype , code , name as gatheringtype_name
                                             from wy_bd_gatheringtype
                                             where dr = 0 "))
 
 
-# ?·???ϸ???ϴ???????ÿִ??һ?????????ڴ沢?ȴ?2????
-print(paste0('chargebills start??' , now()))
-# ---------- Ӧ????ϸ??
-# һ??pk_chargebillsֻ????һ????¼
+# 下方明细表较大，设置每执行一个后清除内存    -- 并等待2分钟
+
+print(paste0('chargebills start: ' , now()))
+
+# ---------- 应收明细表
+# 一个pk_chargebills只会有一条记录
 chargebills <- dbGetQuery(con_orc , glue("select pk_chargebills , pk_house , pk_projectid ,
                                           cost_date , cost_startdate , cost_enddate ,
                                           accrued_amount , dr , line_of , price
                                           from wy_bill_chargebills"))
-print(paste0('chargebills end??' , now()))
+print(paste0('chargebills end: ' , now()))
 gc()
 # Sys.sleep(120)
 
-print(paste0('gathering start??' , now()))
-# ---------- ʵ????ϸ??
+print(paste0('gathering start: ' , now()))
+
+# ---------- 实收明细表
 gathering <- dbGetQuery(con_orc , glue("select pk_gathering , bill_date , dr
                                         from wy_bill_gathering"))
-print(paste0('gathering end??' , now()))
+print(paste0('gathering end: ' , now()))
 gc()
 # Sys.sleep(120)
 
-print(paste0('gathering_d start??' , now()))
+print(paste0('gathering_d start: ' , now()))
+
 gathering_d <- dbGetQuery(con_orc , glue("select pk_gathering , pk_gathering_d , source ,
                                           souse_type , pk_gathering_type , real_amount , dr
                                           from wy_bill_gathering_d"))
-print(paste0('gathering_d end??' , now()))
+print(paste0('gathering_d end: ' , now()))
 gc()
 # Sys.sleep(120)
 
-print(paste0('receive start??' , now()))
-# ---------- ??????ϸ??
+print(paste0('receive start: ' , now()))
+
+# ---------- 减免明细表
 receive <- dbGetQuery(con_orc , glue("select pk_receivable , enableddate , enabled_state , adjust_type , dr
                                       from wy_bd_receivable"))
-print(paste0('receive end??' , now()))
+print(paste0('receive end: ' , now()))
 gc()
 # Sys.sleep(120)
 
-print(paste0('receive_d start??' , now()))
+print(paste0('receive_d start: ' , now()))
+
 receive_d <- dbGetQuery(con_orc , glue("select pk_receivable , pk_receivable_d ,
                                         pk_chargebills , adjust_amount , dr
                                         from wy_bd_receivable_d"))
-print(paste0('receive_d end??' , now()))
+print(paste0('receive_d end: ' , now()))
 gc()
 # Sys.sleep(120)
 
-print(paste0('matchforward start??' , now()))
-# ---------- ??????ϸ??
+print(paste0('matchforward start: ' , now()))
+
+# ---------- 冲抵明细表
 matchforward <- dbGetQuery(con_orc , glue("select pk_forward , pk_recerive ,
                                            pk_gahtering_d , match_amount , dr
                                            from wy_virement_matchforward"))
-print(paste0('matchforward end??' , now()))
+print(paste0('matchforward end: ' , now()))
 gc()
 # Sys.sleep(120)
 
 
-# -------------------- ???????ݺϲ? --------------------
-print(paste0('get data done , wait for processing??' , now()))
+# -------------------- 基础数据合并 --------------------
+print(paste0('get data done , wait for processing: ' , now()))
 
-eve_property <- chargebills %>%   #Ӧ??
+eve_property <- chargebills %>%   #应收
   mutate(COST_STARTDATE = as_date(COST_STARTDATE) ,
          COST_ENDDATE = as_date(COST_ENDDATE)) %>%
   filter(DR == 0 , #COST_ENDDATE >= COST_STARTDATE ,
@@ -96,7 +104,7 @@ eve_property <- chargebills %>%   #Ӧ??
   left_join(basic_info , by = c('PK_HOUSE' = 'pk_house')) %>%
   left_join(belong , by = c('project_name' = 'PORJECT6')) %>%
   select(-DR) %>%
-  left_join(gathering %>%   #ʵ??
+  left_join(gathering %>%   #实收
               rename(bill_time = BILL_DATE) %>%
               mutate(bill_date = as_date(bill_time) ,
                      bill_time = as_datetime(bill_time)) %>%
@@ -107,7 +115,7 @@ eve_property <- chargebills %>%   #Ӧ??
                            select(-DR) %>%
                            rename(gather_souse_type = SOUSE_TYPE) , by = 'PK_GATHERING') , by = c('PK_CHARGEBILLS' = 'SOURCE')) %>%
   left_join(gathering_type , by = c('PK_GATHERING_TYPE' = 'PK_GATHERINGTYPE')) %>%
-  left_join(receive %>%   #????
+  left_join(receive %>%   #减免
               rename(enabledtime = ENABLEDDATE) %>%
               mutate(enableddate = as_date(enabledtime) ,
                      enabledtime = as_datetime(enabledtime) ,
@@ -117,7 +125,7 @@ eve_property <- chargebills %>%   #Ӧ??
               left_join(receive_d %>%
                           filter(DR == 0) %>%
                           select(-DR) , by = c('PK_RECEIVABLE')) , by = c('PK_CHARGEBILLS')) %>%
-  left_join(matchforward %>%   #????
+  left_join(matchforward %>%   #冲抵
               filter(DR == 0) %>%
               select(-DR) %>%
               rename(PK_GATHERING_D = PK_GAHTERING_D) %>%
@@ -129,7 +137,7 @@ eve_property <- chargebills %>%   #Ӧ??
   rename(gatheringtype_code = CODE ,
          forward_souse_type = SOUSE_TYPE)
 
-print(paste0('processing done , wait for fix??' , now()))
+print(paste0('processing done , wait for fix: ' , now()))
 
 names(eve_property) <- tolower(names(eve_property))
 
@@ -155,33 +163,35 @@ eve_property_fix <- eve_property %>%
 
 # names(eve_property_fix)
 
-print(paste0('data done , wait for ETL??' , now()))
+print(paste0('data done , wait for ETL: ' , now()))
 
-# # д??sql server
-# # ʹ?ô˺???д??ʱ????ע????sql server?н??õı????ֶ????ͣ?һ??Ҫ???????ݣ????򱨴?
-# # ʹ?ô˺???ʱ??һ??Ҫ??֤???ݿ?????ͬ?˱????????ֶ?һ?¡?˳??һ?¡?????ƥ?䡢?ֶγ???????Ҫ?󣬷??򱨴?
-# # ?˴??趨append=TRUE????Ϊfalse???˺??????????????ݿ⽨?���?????޸??鷳??????????????ͬ?????ᱨ?���???˽????趨append=TRUE
-# # ??Ϊȫ��????ִ?????ձ??Ĳ???????ִ??????
+# # 写入sql server
+# # 使用此函数写入时，需注意在sql server中建好的表的字段类型，一定要适用数据，否则报错
+# # 使用此函数时，一定要保证数据库中列同此表输出列字段一致、顺序一致、类型匹配、字段长度满足要求，否则报错
+# # 此处设定append=TRUE，若为false，此函数会自行在数据库建表，类型修改麻烦，且若库中已有同名表会报错，因此建议设定append=TRUE
+# # 若为全量，先执行清空表的操作，再执行入库
 # sqlClear(con_sql, 'mid_eve_finance_fee_property')
 # sqlSave(con_sql , eve_property_fix , tablename = "mid_eve_finance_fee_property" ,
 #         append = TRUE , rownames = FALSE , fast = FALSE)
 # 
-# print(paste0('ETL success??' , now()))
+# print(paste0('ETL success: ' , now()))
 # 
-# # ??????????????
+# # 清除工作区内容
 # rm(list = ls())
 # 
 # 
-# # ???ڴ?
-# �????ڿ?ʼ֮ǰ??Ӧ?????????ɴ?????
+# # 清内存
+# gc()
+
+# # 应收结束在开始之前（应收周期生成错误）
 # wrong_1 <- eve_property %>%
 #   filter(COST_STARTDATE > COST_ENDDATE)
 #
-# write.xlsx(wrong_1 , '..\\data\\mid\\eve\\??ҵ??Ӧ?ս????ڿ?ʼ֮ǰ????.xlsx')
+# write.xlsx(wrong_1 , '..\\data\\mid\\eve\\物业费应收结束在开始之前数据?.xlsx')
 #
-# # Ӧ?ն?Ϊ0????ȷ???Ƿ????????ɣ?
+# # 应收额为0（待确认是否金额生成）
 # wrong_2 <- eve_property %>%
 #   filter(ACCRUED_AMOUNT <= 0)
 #
-# write.xlsx(wrong_2 , '..\\data\\mid\\eve\\??ҵ??Ӧ??С??0.xlsx')
+# write.xlsx(wrong_2 , '..\\data\\mid\\eve\\物业费应收小于0.xlsx')
 
