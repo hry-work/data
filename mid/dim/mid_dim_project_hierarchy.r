df885bfdffe233459106b8506614be51ec91e2da
hry-work <18137813799@163.com>
2020-10-15 18:50:30 +0800

    d

---
+++ mid/dim/mid_dim_project_hierarchy.r
@@ -0,0 +1,27 @@
+source('C:/Users/Administrator/data/env.r' , encoding = 'utf8')
+
+author <- c('huruiyi')
+
+# 命名规则: 表应归属库_表类型_归属部门_业务大类_表详细归类
+table <- 'mid_dim_project_hierarchy'      # 项目层级表
+
+project_data <- read.xlsx('..\\data\\mid\\dim\\项目层级表.xlsx' , detectDates = TRUE) %>% 
+  mutate(operate_time = now() ,
+         operate_date = as_date(operate_time) ,
+         handover_date = as_date(handover_date) ,
+         operator = as.character(operator)) %>% 
+  select(id , pk_project , project_name , project5 , project4 , project3 , project2 , 
+         project1 , belong , province , city , county , project_from , handover_date ,
+         is_focus , is_terminate , fdproject_property_a , is_detail_property_a ,
+         fdproject_property_h , is_detail_property_h , fdproject_car , is_detail_car ,
+         fdproject_business , is_detail_business , is_value , operate_date ,
+         operate_time , operator)
+
+
+
+# 入库
+sqlClear(con_sql, table)
+sqlSave(con_sql , project_data , tablename = table ,
+        append = TRUE , rownames = FALSE , fast = FALSE)
+
+print(paste0('ETL project hierarchy success: ' , now()))
