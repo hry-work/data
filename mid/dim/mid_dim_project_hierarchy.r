96efc45faa59a0fdadd5f20ac05211675338ed14
hry-work <18137813799@163.com>
2020-10-26 19:13:24 +0800

    d

--- mid/dim/mid_dim_project_hierarchy.r
+++ mid/dim/mid_dim_project_hierarchy.r
@@ -8,13 +8,10 @@ table <- 'mid_dim_project_hierarchy'      # 项目层级表
 project_data <- read.xlsx('..\\data\\mid\\dim\\项目层级表.xlsx' , detectDates = TRUE) %>% 
   mutate(operate_time = now() ,
          operate_date = as_date(operate_time) ,
-         handover_date = as_date(handover_date) ,
          operator = as.character(operator)) %>% 
-  select(id , project_name , project5 , project4 , project3 , project2 , project1 , 
-         belong , province , city , county , project_from , handover_date ,
-         is_focus , is_terminate , terminate_date , fdproject_property_a , is_detail_property_a ,
-         fdproject_property_h , is_detail_property_h , fdproject_car , is_detail_car ,
-         fdproject_business , is_detail_business , operate_date , operate_time , operator)
+  select(id , project_name , project4 , project3 , project2 , project1 , belong , 
+         csd_project , fdproject_property_a , fdproject_property_h , fdproject_car , 
+         fdproject_business , operate_date , operate_time , operator)
 
 
 
