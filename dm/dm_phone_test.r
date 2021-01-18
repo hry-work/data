7a48d309f8b144e0bfebded2044e016abca617f9
hry-work <18137813799@163.com>
2021-01-18 15:15:18 +0800

    d

--- dm/dm_phone_test.r
+++ dm/dm_phone_test.r
@@ -1,7 +1,7 @@
 print('phone flow start')
 
 # 本地使用
-source('C:/Users/Administrator/data/env.r' , encoding = 'utf8')
+# source('C:/Users/Administrator/data/env.r' , encoding = 'utf8')
 
 # 调度使用
 source('/root/data/env_centos.r' , encoding = 'utf8')
@@ -11,10 +11,18 @@ author <- c('huruiyi')
 print(author)
 
 # 测试从sql server取数
-test <- sqlQuery(con_sqls , "select top 1 pk_project , pk_build , 
-                 pk_unit , pk_floor , pk_house , house_name , build_area
-                 from mid_dim_owner_basic_info") %>% 
-  mutate(d_t = )
+test_write <- sqlQuery(con_sqls , "select top 1 pk_project , pk_build , pk_unit , 
+                       pk_floor , pk_house , house_name , build_area
+                       from mid_dim_owner_basic_info") %>% 
+  mutate(d_t = now())
 
-sqlSave(con_sql , property_fix , tablename = "mid_eve_finance_fee_property" ,
-        #         append = TRUE , rownames = FALSE , fast = FALSE)
+
+# 连接mysql，写入数据
+
+# 连接mysql dm层
+# conn <- dbConnect(con_dm)
+
+# test_2 <- dbGetQuery(conn , "select * from test")
+
+# 写入
+dbWriteTable(con_dm , 'test' , test_write , append = T , row.names = F)
