85a5ae981789472b45bb5f1fdb76430a2d70e3ed
hry-work <18137813799@163.com>
2021-01-18 15:20:12 +0800

    d

--- dm/dm_phone_test.r
+++ dm/dm_phone_test.r
@@ -16,13 +16,16 @@ test_write <- sqlQuery(con_sqls , "select top 1 pk_project , pk_build , pk_unit 
                        from mid_dim_owner_basic_info") %>% 
   mutate(d_t = now())
 
+print('get data success')
 
 # 连接mysql，写入数据
 
 # 连接mysql dm层
-# conn <- dbConnect(con_dm)
+conn <- dbConnect(con_dm)
 
 # test_2 <- dbGetQuery(conn , "select * from test")
 
+print('start write')
+
 # 写入
-dbWriteTable(con_dm , 'test' , test_write , append = T , row.names = F)
+dbWriteTable(conn , 'test' , test_write , append = T , row.names = F)
