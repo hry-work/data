4223b8fd0021cd032aa2e3eb3bebe2ddb5e8ac59
hry-work <18137813799@163.com>
2021-01-22 08:32:10 +0000

    d

--- flow/phone/zip.bat
+++ flow/phone/zip.bat
@@ -1,4 +1,5 @@
 copy phone.flow bak1.flow
 copy phone.flow bak2.flow
 copy phone.flow bak3.flow
-
+del  *.zip
+"C:\Program Files\7-Zip\7z.exe" a phone.zip *.*
--- flow/phone/zip.sh
+++ flow/phone/zip.sh
@@ -1,3 +1,5 @@
 cp phone.flow bak1.flow
 cp phone.flow bak2.flow
 cp phone.flow bak3.flow
+rm -f *.zip
+zip phone.zip *
