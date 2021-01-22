1a75e4cc39a01b78d3d94127d73d1a4287fea73f
hry-work <18137813799@163.com>
2021-01-22 07:28:18 +0000

    d

--- flow/noop/noop_start.r
+++ flow/noop/noop_start.r
@@ -1,4 +1,4 @@
-source('env_centos.r', encoding = 'utf-8')
+source('/root/data/env_centos.r' , encoding = 'utf8')
 
 while (hm(str_c(hour(Sys.time()),minute(Sys.time()), sep=':')) < hm(NOOP_START)) {
 	print(glue('wait to {NOOP_START} am to start'))
