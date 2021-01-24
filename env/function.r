ce53a54a80810a9a76e48d6c50e0a19aa20fdae2
hry-work <18137813799@163.com>
2021-01-24 10:08:02 +0000

    X

--- env/function.r
+++ env/function.r
@@ -1,22 +1,22 @@
 
-# gp <- function(param, sep=','){
-#   # get_param
-#   # 调度时传入参数，-m v='5c481227000004120099265a,5c481227000004120099265a'
-#   # gp('student_id)
-#   if(nrow(DF_MULTI_PARAMS) == 0){
-#     v <- ''
-#   } else {
-#     v <- DF_MULTI_PARAMS %>% filter(k == param) %>% select(v)
-#     if(param == RECENT & nrow(v) == 0){
-#       v <- ''
-#     } else {
-#       v <- v %>% str_split(sep) %>% .[[1]]
-#     }
-#   }
-#   message(param, ':', v)
-#   return(v)
-# }
-# # 'A' %>% str_split(',') %>% .[[1]]
+gp <- function(param, sep=','){
+  # get_param
+  # 调度时传入参数，-m v='5c481227000004120099265a,5c481227000004120099265a'
+  # gp('student_id)
+  if(nrow(DF_MULTI_PARAMS) == 0){
+    v <- ''
+  } else {
+    v <- DF_MULTI_PARAMS %>% filter(k == param) %>% select(v)
+    if(param == RECENT & nrow(v) == 0){
+      v <- ''
+    } else {
+      v <- v %>% str_split(sep) %>% .[[1]]
+    }
+  }
+  message(param, ':', v)
+  return(v)
+}
+# 'A' %>% str_split(',') %>% .[[1]]
 
 
 # 获取给定时间范围落在的对应报表周期
--- env/param.r
+++ env/param.r
@@ -2,11 +2,14 @@
 # RScript path/*.R --start '2018-01-01' --rpt_type 'M'
 # RScript path/*.R -s '2018-01-01' -rt 'M'
 spec = matrix(c(
-  'start',       's',   2, "character",
-  'end'  ,       'e',   2, "character",
-  'pd_type',     'pd',  2, "character", # Y\HY\Q\M\W\D
-  'days' ,       'd',   2, "character",
-  'noop_start',  'n',   2, "character" #
+  'start',        's',   2, "character",
+  'end'  ,        'e',   2, "character",
+  'pd_type',      'pd',  2, "character", # Y\HY\Q\M\W\D
+  'days' ,        'd',   2, "character",
+  'noop_start',   'n',   2, "character", #
+  'multi_params', 'm',   2, "character", # 更多的多个参数，比如p1=v1&p2=v2，谁用了m谁自己解析
+  # 解决${PARAMS未传值为空串''时的错误，"" is not a valid option, or does not support an argument
+  'without_param','w',   2, "character"
 ), byrow=TRUE, ncol=4)
 opt = getopt(spec)
 print(opt)
@@ -21,6 +24,18 @@ is_blank <- function(x){
     return(0)
   } else {
     return(nchar(x))
+  }
+}
+
+message('-m:', opt$multi_params)
+if (is_blank(opt$multi_params) == 0) {
+  DF_MULTI_PARAMS <- data.frame()
+} else {
+  ps <- str_split(opt$multi_params, '&')[[1]]
+  DF_MULTI_PARAMS <- data.frame()
+  for (p in ps) {
+    kv <- str_split(p, '=')[[1]]
+    DF_MULTI_PARAMS <- bind_rows(data.frame(k = kv[1], v = kv[2], stringsAsFactors = FALSE), DF_MULTI_PARAMS)
   }
 }
 
@@ -64,3 +79,9 @@ if (is_blank(opt$noop_start) == 0) {
   NOOP_START <- opt$noop_start
 }
 
+#  -m "recent=15"
+if(gp(RECENT) != ''){
+  start <- today() - as.numeric(gp(RECENT))
+  etl_start <- start
+}
+day <- days[1]
