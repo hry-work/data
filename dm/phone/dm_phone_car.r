73b34f6d915971552879fcf84a7fe914a4602275
hry-work <18137813799@163.com>
2021-01-29 08:30:22 +0000

    car

--- dm/phone/dm_phone_car.r
+++ dm/phone/dm_phone_car.r
@@ -56,25 +56,53 @@ for (day in days) {
            pd_type = 'M' ,
            pd_type_value = month_value ,
            is_complete = if_else(day == month_end , 1 , 0))
+  
+  car_detail <- car_data %>% 
+    filter(cost_datestart <= month_end) %>% 
+    distinct(project_name , pk_chargebills , accrued_amount , projectname) %>% 
+    group_by(project_name , pk_chargebills , projectname) %>% 
+    summarise(accrued_amount = sum(accrued_amount , na.rm = T)) %>% 
+    left_join(car_data %>% 
+                filter(cost_datestart <= month_end) %>% 
+                group_by(project_name , pk_chargebills , projectname) %>% 
+                summarise(real_amount = sum(real_amount[bill_date <= day] , na.rm = T))) %>% 
+    mutate(accrued = round(replace_na(accrued_amount , 0),2) ,
+           real_amount = round(replace_na(real_amount , 0),2) ,
+           project_type = if_else(grepl('临时' , projectname) , '临停' , '非临停' , '非临停')) %>% 
+    group_by(project_name) %>% 
+    summarise(accrued_amount_tempark = sum(accrued[project_type == '临停'] , na.rm = T) ,
+              takeover_amount_tempark = sum(real_amount[project_type == '临停'] , na.rm = T) ,
+              accrued_amount_park = sum(accrued[project_type == '非临停'] , na.rm = T) ,
+              takeover_amount_park = sum(real_amount[project_type == '非临停'] , na.rm = T)) %>% 
+    left_join(car_ratio) %>% 
+    mutate(tax_ratio = replace_na(tax_ratio , 0) ,
+           accrued_amount_tempark_tax = round(accrued_amount_tempark*(1-tax_ratio) , 2) ,
+           takeover_amount_tempark_tax = round(takeover_amount_tempark*(1-tax_ratio) , 2) ,
+           accrued_amount_park_tax = round(accrued_amount_park*(1-tax_ratio) , 2) ,
+           takeover_amount_park_tax = round(takeover_amount_park*(1-tax_ratio) , 2)) %>% 
+    select(-tax_ratio)
 
+  car_d <- car %>% 
+    left_join(car_detail)
+  
   # 替换空值
-  car[is.na(car)] <- 0
+  car_d[is.na(car_d)] <- 0
     
   # 设置重复
-  car <- car %>% 
-    rbind(car %>% 
+  car_d <- car_d %>% 
+    rbind(car_d %>% 
             mutate(ys_end = quarter_end ,
                    get_end = quarter_end ,
                    pd_type = 'Q' ,
                    pd_type_value = quarter_value ,
                    is_complete = if_else(day == quarter_end , 1 , 0))) %>% 
-    rbind(car %>% 
+    rbind(car_d %>% 
             mutate(ys_end = halfyear_end ,
                    get_end = halfyear_end ,
                    pd_type = 'HY' ,
                    pd_type_value = halfyear_value ,
                    is_complete = if_else(day == halfyear_end , 1 , 0))) %>% 
-    rbind(car %>% 
+    rbind(car_d %>% 
             mutate(ys_end = year_end ,
                    get_end = year_end ,
                    pd_type = 'Y' ,
@@ -93,7 +121,7 @@ for (day in days) {
   print('delete success')
   
   # 今日新跑数据写入(windows会报错，带中文的字符串需改为gbk。线上跑数无问题，因此可直接放线上测试)
-  dbWriteTable(conn , table , car , append = T , row.names = F)
+  dbWriteTable(conn , table , car_d , append = T , row.names = F)
   print('write car data success')
   
   # 断开连接
