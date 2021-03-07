473b8a0b977fa7aedf2c67a7fa74e8467912ccb3
hry-work <18137813799@163.com>
2021-03-07 10:24:15 +0000

    D

--- mid/dim/off-mid_dim_project_hierarchy.r
+++ mid/dim/off-mid_dim_project_hierarchy.r
@@ -9,7 +9,7 @@ table <- 'mid_dim_project_hierarchy'      # 项目层级表
 # 直接从sql server数据库内拉取数据(设置了填报，业务更新库内即会更新)
 
 project_data <- read_excel('..\\data\\mid\\dim\\mid_dim_project_hierarchy.xlsx') %>% 
-  mutate(operate_time = if_else(is.na(operate_time) , as.character(now()) , as.character(paste0('2020/11/16 ', '18:02:50'))) ,
+  mutate(operate_time = if_else(is.na(operate_time) , as_datetime(now()) , as_datetime(operate_time)) ,
          operate_date = if_else(is.na(operate_date) , as_date(operate_time) , as_date(operate_date)) ,
          operator = as.character(operator)) %>% 
   select(project_name , project4 , project3 , project2 , project1 , belong , 
@@ -27,7 +27,12 @@ print(paste0('SQL Server ETL project hierarchy success: ' , now()))
 
 
 # oracle入库，注意表名一定要大写
-dbWriteTable(con_orc , 'MID_DIM_PROJECT_HIERARCHY' , project_data , overwrite=TRUE)
+# 先清除数据
+# dbSendUpdate(con_orc , "delete from MID_DIM_PROJECT_HIERARCHY")
+# 写入数据（若overwrite为true，则会新建表，字段类型不符合要求），一直报错，暂还使用老方式让其自动覆盖表
+# dbWriteTable(con_orc , toupper(table) , project_data , overwrite = FALSE, append=TRUE)
+
+dbWriteTable(con_orc , toupper(table) , project_data , overwrite=TRUE)
 
 print(paste0('ORACLE ETL project hierarchy success: ' , now()))
 
